4598c4198887a545f74a42b3ed392d1b167b437d
Victoria <victoriathorsenn@gmail.com>
2021-06-05 00:56:11 -0400

    commit tree 178670efb929b246d6dbd28b286e50b238be5cf8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+const map = (array, func) => {
+    const arr = []
+    for (let i = 0; i < array.length; i++) {
+        let ind = array[i]
+        arr.push(func(ind))
+    }
+    return arr
+}
+
+
+const reduce = (array, func, startingValue) => {
+    let total
+    if (startingValue) {
+        total = startingValue
+        for (let i = 0; i < array.length; i++) {
+            total = func(array[i], total)
+        }
+        return total
+    } else {
+        total = array[0]
+        for (let i = 1; i < array.length; i++) {
+            total = func(array[i], total)
+        }
+        return total
+    }
+}
